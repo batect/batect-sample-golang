c9f94678314bf31ffcde53ad71673415e0a0a721
Charles Korn <me@charleskorn.com>
2019-03-16 18:05:27 -0500

    Log a message when the app is starting.

--- app.go
+++ app.go
@@ -7,8 +7,12 @@ import (
 )
 
 func main() {
+	address := "0.0.0.0:8080"
+
+	log.Infof("Starting on %s...", address)
+
 	http.HandleFunc("/ping", handlePing)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(address, nil))
 }
 
 func handlePing(w http.ResponseWriter, _ *http.Request) {
