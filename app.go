071dfb4196316ce1cf7aae6e80c40c3fb781c0ae
Charles Korn <me@charleskorn.com>
2019-03-16 18:03:30 -0500

    Add a basic HTTP server and unit test.

--- app.go
+++ app.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	"net/http"
 )
 
 func main() {
-	log.Info("Hello world!")
+	http.HandleFunc("/ping", handlePing)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handlePing(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprint(w, "pong")
 }
---
+++ app_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+)
+
+var _ = Describe("The application", func() {
+	Describe("when the ping endpoint is called", func() {
+		var response *http.Response
+
+		BeforeEach(func() {
+			recorder := httptest.NewRecorder()
+
+			handlePing(recorder, nil)
+
+			response = recorder.Result()
+		})
+
+		It("should return 'pong' as the response", func() {
+			body, _ := ioutil.ReadAll(response.Body)
+
+			Expect(string(body)).To(Equal("pong"))
+		})
+
+		It("should return a HTTP 200 status code", func () {
+			Expect(response.StatusCode).To(Equal(http.StatusOK))
+		})
+	})
+})
