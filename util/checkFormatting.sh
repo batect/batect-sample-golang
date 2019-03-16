8899258d66ea4b8637eda06c02e5699a0c4a4c59
Charles Korn <me@charleskorn.com>
2019-03-16 16:21:05 -0500

    Tweak message slightly.

--- util/checkFormatting.sh
+++ util/checkFormatting.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 unformatted_files=$(gofmt -l $(find . -name '*.go' -not -path './.go-cache/*'))
 
 if [ "$unformatted_files" == "" ]; then
-    echo "All files formatted correctly."
+    echo "All files are formatted correctly."
 else
     echo "The following files are not formatted correctly:"
     echo "$unformatted_files"
