dbd2fe2a113fae40133896d37cf080609e08b897
Charles Korn <me@charleskorn.com>
2019-03-16 21:33:19 -0500

    Record and archive test results.

--- app_suite_test.go
+++ app_suite_test.go
@@ -3,10 +3,29 @@ package main
 import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	"os"
+
+	"github.com/onsi/ginkgo/reporters"
 	"testing"
 )
 
 func TestApp(t *testing.T) {
 	RegisterFailHandler(Fail)
-	RunSpecs(t, "App Suite")
+	RunSpecsWithDefaultAndCustomReporters(t, "App Suite", createReporters())
+}
+
+func createReporters() []Reporter {
+	path := os.Getenv("REPORT_PATH")
+
+	if path == "" {
+		return []Reporter{}
+	}
+
+	if err := os.MkdirAll(path, os.ModePerm); err != nil {
+		panic(err)
+	}
+
+	junitReporter := reporters.NewJUnitReporter(path + "/report.xml")
+
+	return []Reporter{junitReporter}
 }
